56563ff2ffc2f3a9d63598d9e6df8ed45c4e60aa
Brian Trammell <brian@trammell.ch>
2017-11-30 11:25:48 +0900

    change ptoload to two-pass metadata scanner, so it can handle metadata postfix input

--- ptoload/ptoload.go
+++ ptoload/ptoload.go
@@ -21,49 +21,53 @@ var helpFlag = flag.Bool("h", false, "display a help message")
 var configFlag = flag.String("config", "ptoconfig.json", "path to PTO configuration `file` with DB connection information")
 var initdbFlag = flag.Bool("initdb", false, "Create database tables on startup")
 
-func PtoLoad(config *pto3.PTOServerConfig, db *pg.DB, r io.Reader) *pto3.ObservationSet {
+func extractMetadata(filename string, r io.Reader) (*pto3.ObservationSet, error) {
+	// create an observation set to hold metadata
+	set := pto3.ObservationSet{}
+
+	// now scan the file for metadata
+	var lineno = 0
+	in := bufio.NewScanner(r)
+	for in.Scan() {
+		lineno++
+		line := strings.TrimSpace(in.Text())
+		if line[0] == '{' {
+			if err := set.UnmarshalJSON([]byte(line)); err != nil {
+				return nil, fmt.Errorf("error in metadata at %s line %d: %s", filename, lineno, err.Error())
+			}
+		}
+	}
+
+	// done
+	return &set, nil
+}
+
+func loadObservations(filename string, db *pg.DB, set *pto3.ObservationSet, r io.Reader) error {
 
 	var lineno = 0
-	var setid int
-	var set pto3.ObservationSet
 
 	in := bufio.NewScanner(r)
 
 	// the rest of this function is all or nothing
 	err := db.RunInTransaction(func(t *pg.Tx) error {
 
-		// start by creating and inserting a dummy set to get a Set ID
-		set = pto3.ObservationSet{}
-
+		// first insert the set
 		if err := set.Insert(t, true); err != nil {
 			return err
 		}
 
-		// save the set ID since UnmarshalJSON will overwrite it
-		setid = set.ID
-
-		// now scan standard input
+		// now scan standard input for observations
 		for in.Scan() {
 			lineno++
 			line := strings.TrimSpace(in.Text())
-			switch line[0] {
-			case '{':
-				// metadata object, update
-				if err := set.UnmarshalJSON([]byte(line)); err != nil {
-					return err
-				}
-				set.ID = setid
-				if err := set.Update(t); err != nil {
-					return err
-				}
-				log.Printf("updated metadata: %v", set)
-			case '[':
-				// observation object
+			if line[0] == '[' {
+				// create an observation object
 				var obs pto3.Observation
 				if err := obs.UnmarshalJSON([]byte(line)); err != nil {
 					return err
 				}
-				if err := obs.InsertInSet(t, &set); err != nil {
+				// and nsert it in the set
+				if err := obs.InsertInSet(t, set); err != nil {
 					return err
 				}
 			}
@@ -71,22 +75,29 @@ func PtoLoad(config *pto3.PTOServerConfig, db *pg.DB, r io.Reader) *pto3.Observa
 		return nil
 	})
 	if err != nil {
-		log.Fatalf("error at input line %d: %v", lineno, err)
+		return fmt.Errorf("error in observation at %s line %d: %s", filename, lineno, err)
 	}
 
-	return &set
+	return nil
 }
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "%s: load observations into a PTO database\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "Usage: %s <flags>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "%s: load observations from a file into a PTO database\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s <flags> input-files\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
 
 	if *helpFlag {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	args := flag.Args()
+
+	if len(args) < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -103,9 +114,31 @@ func main() {
 		}
 	}
 
-	set := PtoLoad(config, db, os.Stdin)
+	for _, filename := range args {
+		func() {
+			obsfile, err := os.Open(filename)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer obsfile.Close()
 
-	log.Printf("created observation set %x:", set.ID)
-	b, _ := json.MarshalIndent(set, "  ", "  ")
-	os.Stderr.Write(b)
+			set, err := extractMetadata(filename, obsfile)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if _, err := obsfile.Seek(0, 0); err != nil {
+				log.Fatal(err)
+			}
+
+			if err := loadObservations(filename, db, set, obsfile); err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("created observation set %x:", set.ID)
+			b, _ := json.MarshalIndent(set, "  ", "  ")
+			os.Stderr.Write(b)
+		}()
+	}
+
 }
