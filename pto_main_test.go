e4f0f90b8fd36987f3515cd8ccf53a70b168bbdb
Brian Trammell <brian@trammell.ch>
2018-04-12 14:48:20 +0200

    clean up after oneself. again.

--- pto_main_test.go
+++ pto_main_test.go
@@ -15,7 +15,7 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-const SuppressDropTables = true
+const SuppressDropTables = false
 const SuppressDeleteRawStore = false
 const SuppressDeleteQueryCache = false
 
