25592dd6dce57b09fbd01ea9ef79626401141674
Brian Trammell <brian@trammell.ch>
2018-08-14 18:00:58 +0200

    refactor ptonorm, notes on autonorm

---
+++ cmd/autonorm/autonorm.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+
+	// get PTO configuration
+
+	// get autonormalizer configuration
+
+	// for each campaign directory
+
+	// for each file
+
+	// skip if deprecated
+
+	// generate a raw file URL
+
+	// find observation sets that have this raw data as a source
+
+	// if no observation sets
+
+	// get autonormalizer for filetype
+
+	// run normalizer into temporary file, then load temporary file
+
+}
---
+++ ptonorm.go
@@ -0,0 +1,185 @@
+package pto3
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func normalizerMetadataCopy(from io.Reader, to io.WriteCloser, errchan chan error) {
+	defer to.Close()
+	buf := make([]byte, 65536)
+	for {
+		n, err := from.Read(buf)
+		if err == nil {
+			_, err2 := to.Write(buf[0:n])
+			if err2 != nil {
+				errchan <- err2
+				log.Fatal(err)
+				return
+			}
+		} else if err == io.EOF {
+			break
+		} else {
+			log.Fatal(err)
+			errchan <- err
+			return
+		}
+	}
+	errchan <- nil
+}
+
+func normalizerMetadataFilter(from io.ReadCloser, to io.Writer, sourceurl string, errchan chan error, donechan chan struct{}) {
+	defer from.Close()
+	scanner := bufio.NewScanner(from)
+	var lineno int
+	md := make(map[string]interface{})
+
+	for scanner.Scan() {
+		lineno++
+		line := strings.TrimSpace(scanner.Text())
+		switch line[0] {
+		case '{':
+			// metadata. coalesce
+			err := json.Unmarshal([]byte(line), &md)
+			if err != nil {
+				errchan <- err
+				return
+			}
+
+		case '[':
+			// data. pass.
+			fmt.Fprintln(to, line)
+		}
+	}
+
+	// At EOF. Add source URL to metadata and emit.
+	md["_sources"] = []string{sourceurl}
+
+	b, err := json.Marshal(md)
+	if err != nil {
+		errchan <- err
+		return
+	}
+	fmt.Fprintf(to, "%s\n", b)
+	errchan <- nil
+	close(donechan)
+}
+
+func RunNormalizer(config *PTOConfiguration, outfile io.Writer,
+	normCmd string, campaign string, filename string) error {
+
+	// create a raw data store (no need for an authorizer)
+	rds, err := NewRawDataStore(config)
+	if err != nil {
+		return err
+	}
+
+	// retrieve the campaign
+	cam, err := rds.CampaignForName(campaign)
+	if err != nil {
+		return err
+	}
+
+	// get metadata for the file
+	md, err := cam.GetFileMetadata(filename)
+	if err != nil {
+		return err
+	}
+
+	// open raw data file
+	rawfile, err := cam.ReadFileData(filename)
+	if err != nil {
+		return err
+	}
+
+	// create subprocess and pipes
+	cmd := exec.Command(normCmd)
+
+	// direct access to datafile
+	cmd.Stdin = rawfile
+
+	// metadata-filtered observations on stdout
+	obspipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	// pass through stderr
+	cmd.Stderr = os.Stderr
+
+	// metadata on fd 3
+	metapipeCmd, metapipe, err := os.Pipe()
+	if err != nil {
+		return err
+	}
+
+	cmd.ExtraFiles = make([]*os.File, 1)
+	cmd.ExtraFiles[0] = metapipeCmd
+
+	// start the command
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	metaerr := make(chan error, 1)
+	// dataerr := make(chan error, 1)
+	obserr := make(chan error, 1)
+	cmderr := make(chan error, 1)
+	outdone := make(chan struct{})
+
+	// get metadata
+	b, err := md.DumpJSONObject(true)
+	if err != nil {
+		return err
+	}
+
+	// start a goroutine to fill the metadata pipe
+	go normalizerMetadataCopy(bytes.NewReader(b), metapipe, metaerr)
+
+	// start a goroutine to fill the data pipe
+	// go copyData(rawfile, datapipe, dataerr)
+
+	// start a goroutine to filter metadata in output
+	// and add a source URL
+	sourceurl := fmt.Sprintf("%s%s/%s/%s", config.BaseURL, "raw", campaign, filename)
+	go normalizerMetadataFilter(obspipe, outfile, sourceurl, obserr, outdone)
+
+	// start a goroutine to wait for the process to finish
+	go func() {
+		cmderr <- cmd.Wait()
+	}()
+
+	// now wait on the exit channels, return as soon as command completes
+	for {
+		select {
+		// case err := <-dataerr:
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		case err := <-metaerr:
+			if err != nil {
+				return err
+			}
+		case err := <-obserr:
+			if err != nil {
+				return err
+			}
+		case err := <-cmderr:
+			if err == nil {
+				// wait on output completion
+				<-outdone
+				return nil
+			} else {
+				return err
+			}
+		}
+	}
+}
